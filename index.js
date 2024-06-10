80a7c941482c54e810d2422648236eef78e131e7
Reem Yacoub <reem.m.yakop@gmail.com>
2024-06-11 00:11:55 +0300

    the online event listener

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // document.getElementById("count-el").innerHTML = 5;
+// Intialize the count as 0
+// Listen for clicks on the increment button 
+// Increment the count variable when the button is clicked 
+// change the count-el n the HTML to reflect he new count
+
+
+increment()
