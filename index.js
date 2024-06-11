87643f7d01d1d1f4ff9b951bec023583885fc1ed
Reem Yacoub <reem.m.yakop@gmail.com>
2024-06-12 01:54:19 +0300

    increment on clicks and display the count

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-// document.getElementById("count-el").innerHTML = 5;
+// document.getElementById("count-el").innerText = 5;
 // Intialize the count as 0
 // Listen for clicks on the increment button 
 // Increment the count variable when the button is clicked 
 // change the count-el n the HTML to reflect he new count
-
-
+let count = 0
+function increment()
+{
+    
+    document.getElementById("count-el").innerText = count 
+    count = count+1
+    // console.log(count)
+}
 increment()
