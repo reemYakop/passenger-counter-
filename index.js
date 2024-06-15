5a4eb152f8ec1832b6a3d7d9504c197082172ae3
Reem Yacoub <reem.m.yakop@gmail.com>
2024-06-16 02:39:23 +0300

    set the count to 0 when you click the save button

--- index.js
+++ index.js
@@ -5,16 +5,20 @@
 // change the count-el n the HTML to reflect he new count
 let count = 0
 let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
 function increment()
 {
     
     count = count+1
     // console.log(count)
-    document.getElementById("count-el").innerText = count 
+    countEl.innerText = count 
 
 }
  function save(){
-    let countStr = " " +count + " -"
-    saveEl.innerText += countStr
+    let countStr = count + " - "
+    saveEl.textContent += countStr
     console.log(count)
+    countEl.textContent = 0
+    count = 0
+    
  }
